37a841c08af87a328a66159e86812fbf20371452
Tony Butler <spudz76@gmail.com>
2020-08-28 15:07:20 -0600

    (no-release) Personal modz pack

--- src/version.h
+++ src/version.h
@@ -52,6 +52,12 @@
 #   endif
 #endif
 
+#if defined(__INTEL_COMPILER)
+#define __INTELC_MAJOR__ ((__INTEL_COMPILER / 100) >> 0)
+#define __INTELC_MINOR__ (((__INTEL_COMPILER - (__INTELC_MAJOR__ * 100)) / 10) >> 0)
+#define __INTELC_PATCH__ (__INTEL_COMPILER - (__INTELC_MAJOR__ * 100) - (__INTELC_MINOR__ * 10))
+#endif
+
 #ifdef XMRIG_OS_WIN
 #    define APP_OS "Windows"
 #elif defined XMRIG_OS_IOS
