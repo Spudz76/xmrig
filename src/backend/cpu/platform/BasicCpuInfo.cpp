2f5ef35f140f48f0e13d24a8822b53fe106e8df2
Tony Butler <spudz76@gmail.com>
2021-11-28 16:42:17 -0700

    Fix unused count_limit4 when WITH_CN_HEAVY is OFF

--- src/backend/cpu/platform/BasicCpuInfo.cpp
+++ src/backend/cpu/platform/BasicCpuInfo.cpp
@@ -328,7 +328,9 @@ xmrig::CpuThreads xmrig::BasicCpuInfo::threads(const Algorithm &algorithm, uint3
     const uint32_t count = std::thread::hardware_concurrency();
     const uint32_t count_limit  = std::max(static_cast<uint32_t>(count * (limit / 100.0f)), 1U);
     const uint32_t count_limit2 = std::max(static_cast<uint32_t>(count / 2), count_limit);
+#   ifdef XMRIG_ALGO_CN_HEAVY
     const uint32_t count_limit4 = std::max(static_cast<uint32_t>(count / 4), count_limit);
+#   endif
 
     if (count == 1) {
         return 1;
